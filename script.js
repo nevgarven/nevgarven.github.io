76562348f42cf39b721d2bb79e5599c5fe4c8b06
Nev Garven <nev.garven@gmail.com>
2025-03-13 13:06:46 +1300

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,6 @@ function StripCollection(strips) {
  }
 
 // Strip array horizontal images
-var hsh = new StripCollection(["20250221_Pete.png","donutH.png","beansH.png","branchingH.png","negativeH.png","bassH.png","bubbleH.png","thoughtsH.png","telepathyH.png","curseH.png","blinkH.png","toothpasteH.png","notcomingH.png","lateH.png","tangledH.png","cuteH.png", "biteH.png", "eyeH.png", "alligatorH.png", "toeH.png", "cupcakeH.png", "whaleH.png", "hatH.png", "VoiceH.png", "switchH.png", "sunbrellaH.png", "badlyDrawnH.png", "ispyH.png", "winnersH.png"]);
+var hsh = new StripCollection(["20250221_Pete.png","20250225_online","20250228_Winners"]);
 // Strip array vertical images
-var hsv = new StripCollection(["20250221_Pete.png","donutV.png","beansV.png","branchingV.png","negativeV.png","bassV.png","bubbleV.png","thoughtsV.png","telepathyV.png","curseV.png","blinkV.png","toothpasteV.png","notcomingV.png","lateV.png","tangledV.png","cuteV.png", "biteV.png", "eyeV.png", "alligatorV.png", "toeV.png", "cupcakeV.png", "whaleV.png", "hatV.png", "VoiceV.png", "switchV.png", "sunbrellaV.png", "badlyDrawnV.png", "ispyV.png", "winnersV.png"]);
+var hsv = new StripCollection(["20250221_Pete.png","20250225_online","20250228_Winners"]);
